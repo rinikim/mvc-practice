da66d3f7915801e5f3818d69fb10b704c65c64f1
rinikim <tytntu@naver.com>
2022-11-11 21:57:07 +0900

    feat : 클레스타입 객체 가져오는 방법 3가지 테스트 해보기

--- src/test/java/org/example/ReflectionTest.java
+++ src/test/java/org/example/ReflectionTest.java
@@ -15,6 +15,8 @@ import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 /**
  * @Controller 이 설정돼있는 모든 클래스를 찾아서 출력한다.
  */
@@ -45,5 +47,28 @@ public class ReflectionTest {
         logger.debug("User all declared fields: [{}]", Arrays.stream(clazz.getDeclaredFields()).collect(Collectors.toList()));
         logger.debug("User all declared constructors: [{}]", Arrays.stream(clazz.getDeclaredConstructors()).collect(Collectors.toList()));
         logger.debug("User all declared methods: [{}]", Arrays.stream(clazz.getDeclaredMethods()).collect(Collectors.toList()));
+    }
+
+    @Test
+    void load() throws ClassNotFoundException {
+        // 클레스타입 객체 가져오는 방법 3가지
+        // 1. 힙 영역에 로드되어있는 클래스타입의 객체를 가져온다.
+        Class<User> clazz = User.class;
+
+        // 2. 객체를 우선 생성하여 instance.getClass()를 사용하여 객체를 가져온다.
+        User user = new User("tytntu", "김혜린");
+        Class<? extends User> clazz2 = user.getClass();
+
+        // 3. Class.forName("클래스 경로")
+        Class<?> clazz3 = Class.forName("org.example.model.User");
+
+        logger.debug("clazz : [{}]", clazz);
+        logger.debug("clazz2 : [{}]", clazz2);
+        logger.debug("clazz3 : [{}]", clazz3);
+
+        assertThat(clazz == clazz2).isTrue();
+        assertThat(clazz3 == clazz2).isTrue();
+        assertThat(clazz3 == clazz).isTrue();
+
     }
 }
